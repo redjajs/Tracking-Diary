96a99af78fdfdf237664a6c5e807cf7e8168e480
redjajs <131351247+redjajs@users.noreply.github.com>
2023-04-27 11:27:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,23 @@ from yaml.loader import SafeLoader
 with open('../config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
     
-authenticator = Authenticate(
+authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-    config['preauthorized']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')    
+fullname, authentication_status, username = authenticator.login('Login', 'main')  
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
 
 # Funktion zum Laden der Daten aus der JSON-Datei
 def load_data(filename):
