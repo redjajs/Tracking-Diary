c4a457eecefbb19209f988ac6efe65cbd7cd777c
redjajs <131351247+redjajs@users.noreply.github.com>
2023-04-27 11:23:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,20 @@ import time
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+
+
 with open('../config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
+    
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')    
 
 # Funktion zum Laden der Daten aus der JSON-Datei
 def load_data(filename):
