4e95cf91cb131817f1f06fd20b900c4670216b26
redjajs <131351247+redjajs@users.noreply.github.com>
2023-05-11 18:08:56 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -11,12 +11,35 @@ import yaml
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 
+
+
+tips = {
+    "hoher Blutdruck": [
+        "Vermeide salzhaltige Nahrung",
+        "Vermeide Koffein",
+        "Vermeide k\u00f6rperliche Anstrengung in der n\u00e4chsten Stunde",
+        "Trinke Zitronenwasser",
+        "Achten Sie auf eine ausgewogene Ern\u00e4hrung mit viel Obst und Gem\u00fcse."
+    ],
+    "Niedriger Blutdruck": [
+        "Konsumiere salzhaltige Nahrung",
+        "Konsumiere Koffein",
+        " Vermeiden Sie pl\u00f6tzliches Aufstehen, um Schwindelgef\u00fchle zu vermeiden.",
+        "Tragen Sie Kompressionsstr\u00fcmpfe, um den Blutfluss zu verbessern.",
+        "Trinken Sie viel, um Ihren Fl\u00fcssigkeitshaushalt aufrechtzuerhalten."
+    ]
+}
+
+
+
+
+
+
+
 # -------- load secrets for jsonbin.io --------
 jsonbin_secrets = st.secrets["jsonbin"]
 api_key = jsonbin_secrets["api_key"]
 bin_id = jsonbin_secrets["bin_id"]
-
-st.write(jsonbin_secrets)
 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -62,7 +85,8 @@ def save_data(data, filename):
         json.dump(data, file, indent=2, ensure_ascii=False)
 
 # Daten werden geladen und ins DataFrame konvertiert.
-tracked_data = load_data("blutdruck.json")
+#tracked_data = load_data("blutdruck.json")
+tracked_data = load_key(api_key, bin_id, username)
 tracked_data = pd.DataFrame(tracked_data)
 
 # Benachrichtigung mithilfe einer Checkbox in der Sidebar. Die Benachrichtigung soll nicht länger als 3 sekunden eingeblendet werden.
@@ -120,9 +144,11 @@ with tab1:
         new_data = pd.DataFrame(new_data)
         # Pandas wurde benutzt, um die neuen daten an bestehende Daten als neue Zeile anzuhängen.
         df = pd.concat([tracked_data, new_data], ignore_index=True)
+        df = df.fillna('')
         # Umwandlung zurück zu Dictionary
         data_dict = df.to_dict(orient="records")
-        save_data(data_dict, "blutdruck.json")
+        save_key(api_key, bin_id, username, data_dict)
+        #save_data(data_dict, "blutdruck.json")
         st.info("Die Daten wurden gespeichert.")
 
 # Zweite Seite der App: Tagebuch, hier werden die Messwerte gespeichert.
@@ -165,71 +191,75 @@ with tab2:
 # Dritte Seite der App: Hier werden Empfehlungen für den User vorgeschlagen.
 with tab3:
     st.header('_:orange[Tipps]_:bulb:')
-    # Tipps aus JSON-Datei laden
-    tips_data = load_data('tips.json')
-    if tips_data:
-        for key, value in tips_data.items():
-            st.subheader(key)
-            for tip in value:
-                st.write(f"- {tip}")
+    st.write(tips)
+    
+    # # Tipps aus JSON-Datei laden
+    # tips_data = load_data('tips.json')
+    # if tips_data:
+    #     for key, value in tips_data.items():
+    #         st.subheader(key)
+    #         for tip in value:
+    #             st.write(f"- {tip}")
 
-# Tipps für einen zu hohen Blutdruck
-if 'Too High Blood Pressure' in tips_data:
-    tips_data['Too High Blood Pressure'] = ['Vermeide salzhaltige Nahrung', 'Vermeide Koffein','Vermeide körperliche Anstrengung in der nächsten Stunde','Trinke Zitronenwasser','Achten Sie auf eine ausgewogene Ernährung mit viel Obst und Gemüse.']
+# # Tipps für einen zu hohen Blutdruck
+# if 'Too High Blood Pressure' in tips_data:
+#     tips_data['Too High Blood Pressure'] = ['Vermeide salzhaltige Nahrung', 'Vermeide Koffein','Vermeide körperliche Anstrengung in der nächsten Stunde','Trinke Zitronenwasser','Achten Sie auf eine ausgewogene Ernährung mit viel Obst und Gemüse.']
 
 
-# Schreiben der modifizierten JSON-Daten in die Datei.
-with open('tips.json', 'w') as json_file:
-    json.dump(tips_data, json_file, indent=4)
+# # Schreiben der modifizierten JSON-Daten in die Datei.
+# with open('tips.json', 'w') as json_file:
+#     json.dump(tips_data, json_file, indent=4)
     
     
-with open('tips.json', 'r') as json_file:
-    tips_data = json.load(json_file)
+# with open('tips.json', 'r') as json_file:
+#     tips_data = json.load(json_file)
 
-# Laden der JSON-Daten
-# Tipps für einen niedrigen Blutdruck
-if 'Too Low Blood Pressure' in tips_data:
-    tips_data['Too Low Blood Pressure'] = ['Konsumiere salzhaltige Nahrung', 'Konsumiere Koffein',' Vermeiden Sie plötzliches Aufstehen, um Schwindelgefühle zu vermeiden.', 'Tragen Sie Kompressionsstrümpfe, um den Blutfluss zu verbessern.','Trinken Sie viel, um Ihren Flüssigkeitshaushalt aufrechtzuerhalten.']
-
-
-# Schreiben der modifizierten JSON-Daten in die Datei
-with open('tips.json', 'w') as json_file:
-    json.dump(tips_data, json_file, indent=4)
-
-# Laden der JSON-Daten
-with open('tips.json', 'r') as json_file:
-    tips_data = json.load(json_file)
+# # Laden der JSON-Daten
+# # Tipps für einen niedrigen Blutdruck
+# if 'Too Low Blood Pressure' in tips_data:
+#     tips_data['Too Low Blood Pressure'] = ['Konsumiere salzhaltige Nahrung', 'Konsumiere Koffein',' Vermeiden Sie plötzliches Aufstehen, um Schwindelgefühle zu vermeiden.', 'Tragen Sie Kompressionsstrümpfe, um den Blutfluss zu verbessern.','Trinken Sie viel, um Ihren Flüssigkeitshaushalt aufrechtzuerhalten.']
 
 
+# # Schreiben der modifizierten JSON-Daten in die Datei
+# with open('tips.json', 'w') as json_file:
+#     json.dump(tips_data, json_file, indent=4)
 
-#Titel des JSON Inhalts wird geändert.
-# JSON-Datei laden
-with open('tips.json', 'r') as json_file:
-    tips_data = json.load(json_file)
-
-
-# Titel ändern 1: von ,,Too High Blood Pressure'' zu ,,Hoher Blutdruck''.
-if 'Too High Blood Pressure' in tips_data:
-    tips_data['hoher Blutdruck'] = tips_data.pop('Too High Blood Pressure')
-    # Hier wird der Titel von 'Too High Blood Pressure' zu 'Hoher Blutdruck' geändert.
-
-# Modifizierte JSON-Datei wird gespeichert.
-with open('tips.json', 'w') as json_file:
-    json.dump(tips_data, json_file, indent=4)
+# # Laden der JSON-Daten
+# with open('tips.json', 'r') as json_file:
+#     tips_data = json.load(json_file)
 
 
+# #Titel des JSON Inhalts wird geändert.
+# # JSON-Datei laden
+# with open('tips.json', 'r') as json_file:
+#     tips_data = json.load(json_file)
 
-#Titel ändern 2 von ,,Too Low Blood Pressure'' zu ,,Niederiger Blutdruck''.
-if 'Too Low Blood Pressure' in tips_data:
-    tips_data['Niedriger Blutdruck'] = tips_data.pop('Too Low Blood Pressure')
-    # Hier wird der Titel von 'Too Low Blood Pressure' zu 'Niedriger BLutdruck' geändert.
+
+# # Titel ändern 1: von ,,Too High Blood Pressure'' zu ,,Hoher Blutdruck''.
+# if 'Too High Blood Pressure' in tips_data:
+#     tips_data['hoher Blutdruck'] = tips_data.pop('Too High Blood Pressure')
+#     # Hier wird der Titel von 'Too High Blood Pressure' zu 'Hoher Blutdruck' geändert.
+
+# # Modifizierte JSON-Datei wird gespeichert.
+# with open('tips.json', 'w') as json_file:
+#     json.dump(tips_data, json_file, indent=4)
 
 
-# Modifizierte JSON-Datei wird gespeichert.
-with open('tips.json', 'w') as json_file:
-    json.dump(tips_data, json_file, indent=4)
+
+# #Titel ändern 2 von ,,Too Low Blood Pressure'' zu ,,Niederiger Blutdruck''.
+# if 'Too Low Blood Pressure' in tips_data:
+#     tips_data['Niedriger Blutdruck'] = tips_data.pop('Too Low Blood Pressure')
+#     # Hier wird der Titel von 'Too Low Blood Pressure' zu 'Niedriger BLutdruck' geändert.
+
+
+# # Modifizierte JSON-Datei wird gespeichert.
+# with open('tips.json', 'w') as json_file:
+#     json.dump(tips_data, json_file, indent=4)
 
 
 #Optik Design
 st.write("---")
 st.text('© 2023 Applaunch Wädenswil ZHAW')
+
+
+
